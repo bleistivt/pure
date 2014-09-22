4ecb334885a3acb71708af81981c827bbce1dea3
bleistivt <nmh.acww@yahoo.de>
2014-09-22 04:15:49 +0200

    version bump

--- about.php
+++ about.php
@@ -1,10 +1,10 @@
 <?php
 $ThemeInfo['pure'] = array(
-  'Name'        => 'Pure',
-  'Description' => "Pure strips everything unnecessary out of the default theme and uses components of the YUI Pure CSS framework to deliver a simple and clean theme.",
-  'Version'     => '1.0',
-  'Url'         => 'https://github.com/bleistivt/vanilla-pure',
-  'Author'      => 'Bleistivt',
-  'AuthorUrl'   => 'http://bleistivt.net',
-  'RequiredApplications' => array('Vanilla' => '2.1'),
+    'Name'        => 'Pure',
+    'Description' => "Pure strips everything unnecessary out of the default theme and uses components of the YUI Pure CSS framework to deliver a simple and clean theme.",
+    'Version'     => '1.1',
+    'Url'         => 'https://github.com/bleistivt/vanilla-pure',
+    'Author'      => 'Bleistivt',
+    'AuthorUrl'   => 'http://bleistivt.net',
+    'RequiredApplications' => array('Vanilla' => '2.1')
 );
