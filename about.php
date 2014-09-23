2ba4f34773c18ff06ba35fc3de5f407ec90d654e
bleistivt <nmh.acww@yahoo.de>
2014-09-23 02:11:28 +0200

    fix table views

--- about.php
+++ about.php
@@ -2,7 +2,7 @@
 $ThemeInfo['pure'] = array(
     'Name'        => 'Pure',
     'Description' => "Pure strips everything unnecessary out of the default theme and uses components of the YUI Pure CSS framework to deliver a simple and clean theme.",
-    'Version'     => '1.1',
+    'Version'     => '1.2',
     'Url'         => 'https://github.com/bleistivt/vanilla-pure',
     'Author'      => 'Bleistivt',
     'AuthorUrl'   => 'http://bleistivt.net',
